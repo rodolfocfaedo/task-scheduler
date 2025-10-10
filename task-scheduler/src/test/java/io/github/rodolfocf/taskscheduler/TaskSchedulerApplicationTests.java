d8683722ac9803b7cf71b15dde9d136a32b87f95
Rodolfo Cunhasque Faedo <faedorodolfo.programming@gmail.com>
2025-10-10 11:17:15 -0300

    added task scheduler module

---
+++ task-scheduler/src/test/java/io/github/rodolfocf/taskscheduler/TaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rodolfocf.taskscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
